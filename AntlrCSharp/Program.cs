88538f4f9ef9dfca87cc7452fedba2b94e47ed16
DwiN3 <dwinicht333@gmail.com>
2024-04-20 13:19:54 +0200

    Dodanie wersji z drzewem

--- AntlrCSharp/Program.cs
+++ AntlrCSharp/Program.cs
@@ -2,15 +2,22 @@
 using Antlr4.Runtime;
 using AntlrCSharp;
 
-Console.WriteLine("Wpisz operacje matematyczną:");
+Console.WriteLine("Podaj wyrażenie matematyczne:");
 string input = Console.ReadLine();
 
-AntlrInputStream inputStream = new AntlrInputStream(input);
-MathLexer lexer = new MathLexer(inputStream);
-CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-MathParser parser = new MathParser(tokenStream);
+// Tworzymy obiekt analizatora i leksera
+var lexer = new MathLexer(new Antlr4.Runtime.AntlrInputStream(input));
+var tokens = new Antlr4.Runtime.CommonTokenStream(lexer);
+var parser = new MathParser(tokens);
 
-IParseTree tree = parser.expression();
-MathVisitor visitor = new MathVisitor();
-double result = visitor.Visit(tree);
-Console.WriteLine($"Result: {result}");
+// Pobieramy korzeń drzewa składniowego
+var expressionContext = parser.expression();
+
+// Wyświetlamy drzewo parsowania
+Console.WriteLine(expressionContext.ToStringTree(parser));
+
+// Tworzymy obiekt wizytora i odwiedzamy korzeń drzewa składniowego
+var visitor = new MathVisitor();
+double result = visitor.Visit(expressionContext);
+
+Console.WriteLine($"Wynik: {result}");
